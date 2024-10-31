33866a2d0e19c103ce19afdeaa2bf43ed6a5e71e
MohammadHossein Haghdadi <mohammad.haghdadi@divar.ir>
2024-10-31 22:40:06 +0330

    draft

--- cmd/ideyar/serve.go
+++ cmd/ideyar/serve.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	livenessImp "github.com/mosaic-2/IdeYar-server/internal/servicers/liveness"
+	"github.com/mosaic-2/IdeYar-server/pkg/LivenessService"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
 	"net"
+	"net/http"
 
 	"google.golang.org/grpc"
 )
@@ -13,13 +20,35 @@ func serve() error {
 		return fmt.Errorf("failed to listen: %v", err)
 	}
 
-	grpcServer := grpc.NewServer(grpc.UnaryInterceptor(interceptor.MiddleWareAuth()))
+	grpcServer := grpc.NewServer()
 
-	server := servicer.NewServer()
-	server.RegisterUserAPIServer(grpcServer, server)
+	livenessServer, err := livenessImp.NewServer()
+	if err != nil {
+		return fmt.Errorf("faild to init liveness server %v", err)
+	}
+	LivenessService.RegisterLivenessServer(grpcServer, livenessServer)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		return fmt.Errorf("failed to serve: %v", err)
 	}
+
 	return nil
+}
+
+func run() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+	// Register gRPC-Gateway handlers
+	err := LivenessService.RegisterLivenessHandlerFromEndpoint(ctx, mux, "localhost"+*grpcPort, []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())})
+	if err != nil {
+		log.Fatalf("Failed to register gRPC-Gateway: %v", err)
+	}
+
+	log.Printf("Starting HTTP server on %s...", *httpPort)
+	if err := http.ListenAndServe(*httpPort, mux); err != nil {
+		log.Fatalf("Failed to serve HTTP server: %v", err)
+	}
 }
