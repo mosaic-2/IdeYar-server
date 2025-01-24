adb33726302da495d0c6d0efdc3e66c4eb3f5efc
MohammadHossein Haghdadi <mohammad.haghdadi@divar.ir>
2025-01-24 17:26:23 +0330

    fix

--- internal/interceptor/interceptor.go
+++ internal/interceptor/interceptor.go
@@ -27,7 +27,6 @@ func AuthMiddleware(next http.Handler) http.Handler {
 			"/api/search-post",
 			"/auth/forget-password",
 			"/auth/forget-password-finalize",
-			"/api/post",
 		}
 
 		for _, method := range allowedMethods {
